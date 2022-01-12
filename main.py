4191130da49fdedc1e2a32e4a871fe89f5e50621
Zahar <tuzat369@gmail.com>
2022-01-12 11:27:45 +0300

    add letter p

---
+++ main.py
@@ -0,0 +1,15 @@
+def p():
+    return [
+'pppppppp  ',
+'ppppppppp ',
+'ppp    ppp',
+'ppp    ppp',
+'ppp    ppp',
+'ppppppppp ',
+'pppppppp  ',
+'ppp       ',
+'ppp       ',
+'ppp       ',
+'ppp       ',
+'ppp       '
+        ]

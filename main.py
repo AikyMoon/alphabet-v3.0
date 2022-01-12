ebb78d39aac27a6d746740ec4f0d97cf7f8f740c
Zahar <tuzat369@gmail.com>
2022-01-12 11:42:44 +0300

    add letter s

---
+++ main.py
@@ -0,0 +1,15 @@
+def s():
+    return [
+'  sssssss ',
+' sssssssss',
+' sss      ',
+'sss       ',
+'sss       ',
+' ssssss   ',
+'  sssssss ',
+'   sssssss',
+'       sss',
+'      sss ',
+'sssssssss ',
+' sssssss  '
+        ]

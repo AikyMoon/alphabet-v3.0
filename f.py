301b74ff88424f7963bb6cf435d8a7bc4716852d
Zahar <tuzat369@gmail.com>
2022-01-12 10:41:56 +0300

    add letter f

---
+++ f.py
@@ -0,0 +1,15 @@
+def f():
+    return [
+        'ffffffffff',
+        'ffffffffff',
+        'ff        ',
+        'ff        ',
+        'ff        ',
+        'fffffff   ',
+        'fffffff   ',
+        'ff        ',
+        'ff        ',
+        'ff        ',
+        'ff        ',
+        'ff        '
+    ]

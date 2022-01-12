2168519e3d34d3197af9735060b1c1790cb434af
Zahar <tuzat369@gmail.com>
2022-01-12 10:44:53 +0300

    add letter i

---
+++ i.py
@@ -0,0 +1,15 @@
+def i():
+    return [
+        'iiiiiiiiii',
+        'iiiiiiiiii',
+        '    ii    ',
+        '    ii    ',
+        '    ii    ',
+        '    ii    ',
+        '    ii    ',
+        '    ii    ',
+        '    ii    ',
+        '    ii    ',
+        'iiiiiiiiii',
+        'iiiiiiiiii',
+    ]

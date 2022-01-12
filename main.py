008c47cb94f74d7f6a42d4f5c01182e7260c1d25
Zaharic <88633850+Zaharic@users.noreply.github.com>
2022-01-12 11:07:06 +0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+letters_dict = {
+    'a' : a,
+    'b' : b,
+    'c' : c,
+    'd' : d,
+    'e' : e,
+    'f' : f,
+    'g' : g,
+    'h' : h,
+    'i' : i,
+    'j' : j,
+    'k' : k,
+    'l' : l,
+    'm' : m,
+    'n' : n,
+    'o' : o,
+    'p' : p,
+    'q' : q,
+    'r' : r,
+    's' : s,
+    't' : t,
+    'u' : u,
+    'v' : v,
+    'w' : w,
+    'x' : x,
+    'y' : y,
+    'z' : z,
+    ' ' : void,
+}
+
+def parser(text):
+    parse_text = []
+    for let in text.lower():
+        parse_text.append(let)
+    return parse_text
+
+def print_letters(parse:list, lets:dict):
+    word = []
+    for let in parse:
+        word.append(lets[let]())
+
+    for i in range(12):
+        for j in range(len(word)):
+            print(word[j][i], end = '  ')
+        print()
+
+text = input()
+
+lst = parser(text)
+print_letters(lst, let_dict)

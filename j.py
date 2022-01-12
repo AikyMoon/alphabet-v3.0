e981fea9ff21318ddd6a688b91c921060ab814ca
Zahar <tuzat369@gmail.com>
2022-01-12 10:45:46 +0300

    add letter j

---
+++ j.py
@@ -0,0 +1,15 @@
+def j():
+    return [
+        'jjjjjjjjjj',
+        'jjjjjjjjjj',
+        '    jj    ',
+        '    jj    ',
+        '    jj    ',
+        '    jj    ',
+        '    jj    ',
+        '    jj    ',
+        'j   jj    ',
+        'jj  jj    ',
+        ' jj jj    ',
+        '  jjj      '
+    ]

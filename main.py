839e170099040bf0db3405b7cc54b0fe2046b082
Zahar <tuzat369@gmail.com>
2022-01-12 11:52:22 +0300

    add letter y

---
+++ main.py
@@ -0,0 +1,15 @@
+def y():
+    return [
+        'yyy    yyy',
+        'yyy    yyy',
+        ' yyy  yyy ',
+        ' yyy  yyy ',
+        '  yyyyyy  ',
+        '   yyyy   ',
+        '   yyyy   ',
+        '   yyyy   ',
+        '    yy    ',
+        '    yy    ',
+        '    yy    ',
+        '    yy    '
+    ]

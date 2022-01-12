a054ae2cfce6d217a2e25a35e688e15c8881972e
Zahar <tuzat369@gmail.com>
2022-01-12 10:49:25 +0300

    add letter m

---
+++ m.py
@@ -0,0 +1,15 @@
+def m():
+    return [
+        'mm      mm',
+        'mm      mm',
+        'm m    m m',
+        'm m    m m',
+        'm  m  m  m',
+        'm  m  m  m',
+        'm   mm   m',
+        'm        m',
+        'm        m',
+        'm        m',
+        'm        m',
+        'm        m'
+    ]

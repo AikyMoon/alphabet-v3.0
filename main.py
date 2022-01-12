3272937d4c52edeecae2e5da6698a343b227b6e1
Zahar <tuzat369@gmail.com>
2022-01-12 10:55:17 +0300

    rename

---
+++ main.py
@@ -0,0 +1,15 @@
+def a():
+    return[
+        '    aa    ',
+        '    aa    ',
+        '   a  a   ',
+        '   a  a   ',
+        '   a  a   ',
+        '  a    a  ',
+        '  a    a  ',
+        '  aaaaaa  ',
+        ' a      a ',
+        ' a      a ',
+        'a        a',
+        'a        a'
+    ]

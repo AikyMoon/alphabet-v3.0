9df941cc1dec1e1fdba05d96a507565a4502d80d
Zahar <tuzat369@gmail.com>
2022-01-12 11:40:26 +0300

    add letter r

---
+++ main.py
@@ -0,0 +1,15 @@
+def r():
+    return [
+        'rrrrrrrrrrr ',
+        'rr        rr',
+        'rr        rr',
+        'rr        rr',
+        'rr        rr',
+        'rrrrrrrrrrrr',
+        'rr          ',
+        'rrrr        ',
+        'rr  rrr     ',
+        'rr     rr   ',
+        'rr       rr ',
+        'rr        rr'
+    ]

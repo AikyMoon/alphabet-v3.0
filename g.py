4552937d5089f18b9fd2f073b0baf94eaf81e848
Zahar <tuzat369@gmail.com>
2022-01-12 10:42:58 +0300

    add letter g

---
+++ g.py
@@ -0,0 +1,15 @@
+def g():
+    return [
+        '   ggggg  ',
+        '  ggggggg ',
+        ' gg     gg',
+        'gg      gg',
+        'gg        ',
+        'gg        ',
+        'gg        ',
+        'gg   ggggg',
+        'gg      gg',
+        ' gg     gg',
+        '  ggggggg ',
+        '   ggggg  ',
+    ]

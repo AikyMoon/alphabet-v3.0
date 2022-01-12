8bfb874b72b3eef57040088ac6159d401b150c80
Zahar <tuzat369@gmail.com>
2022-01-12 10:40:28 +0300

    add letter e

---
+++ e.py
@@ -0,0 +1,16 @@
+
+def e():
+    return [
+        'eeeeeeeeee',
+        'eeeeeeeeee',
+        'ee     eee',
+        'ee        ',
+        'ee        ',
+        'eeeeeee   ',
+        'eeeeeee   ',
+        'ee        ',
+        'ee        ',
+        'ee     eee',
+        'eeeeeeeeee',
+        'eeeeeeeeee',
+    ]

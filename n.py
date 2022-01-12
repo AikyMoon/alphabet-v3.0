f534180ce4e036f190a6530f1b218c864473f2ef
Zahar <tuzat369@gmail.com>
2022-01-12 10:50:31 +0300

    add letter n

---
+++ n.py
@@ -0,0 +1,15 @@
+def n():
+    return [
+        'nnn    nnn',
+        'nnn    nnn',
+        'nnnn   nnn',
+        'nnnnn  nnn',
+        'nnnnn  nnn',
+        'nnn nn nnn',
+        'nnn nn nnn',
+        'nnn  nnnnn',
+        'nnn  nnnnn',
+        'nnn   nnnn',
+        'nnn    nnn',
+        'nnn    nnn'
+    ]

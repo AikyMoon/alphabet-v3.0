da1adfbeb048f81ed165d153c79e9dc886e811e7
Zahar <tuzat369@gmail.com>
2022-01-12 10:46:55 +0300

    add letter k

---
+++ k.py
@@ -0,0 +1,15 @@
+def k():
+    return [
+        'k      k  ',
+        'k     k   ',
+        'k    k    ',
+        'k   k     ',
+        'k  k      ',
+        'kkk       ',
+        'kk k      ',
+        'k   k     ',
+        'k    k    ',
+        'k     k   ',
+        'k     k   ',
+        'k      k  '
+    ]

a4d37c775c1f91ca0a959b599f3e50b3a2d52177
Zahar <tuzat369@gmail.com>
2022-01-12 11:48:46 +0300

    add letter w

---
+++ main.py
@@ -0,0 +1,15 @@
+def w():
+    return [
+        'ww      ww',
+        'ww      ww',
+        'ww      ww',
+        'ww      ww',
+        'ww      ww',
+        'ww      ww',
+        'ww  ww  ww',
+        'ww wwww ww',
+        'wwww  wwww',
+        'wwww  wwww',
+        'www    www',
+        'ww      ww'
+    ]
